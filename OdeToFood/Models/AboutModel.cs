6592512e58efefbc713f432661f6080815acc998
conoroneill136 <conoroneill136@gmail.com>
2019-03-20 16:27:47 +0000

    de initialized submodule in attempt to fix OdeToFood solution

---
+++ OdeToFood/Models/AboutModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.Models
+{
+    public class AboutModel
+    {
+        public String Name { get; set; }
+        public String Location { get; set; }
+    }
+}
